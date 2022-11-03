4e0535e1422f1d07ef9684667d6671434cafc934
Chip O'Connor <chip@ibchip.com>
2022-11-03 16:14:04 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chip O'Connor on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
